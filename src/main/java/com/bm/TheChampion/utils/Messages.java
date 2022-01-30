8e0290c3e1218c163f1cae82d5aa40964cea1474
ranahamed <ranahamed6193@yahoo.com>
2022-01-30 23:07:28 +0200

    editing grouping participants messages

--- src/main/java/com/bm/TheChampion/utils/Messages.java
+++ src/main/java/com/bm/TheChampion/utils/Messages.java
@@ -8,7 +8,7 @@ public class Messages {
     public static final String WRONG_GROUP_NUMBER = "Number of groups can't be less than 1";
     public static final String PARTICIPANT_DIVIDED = "particpants are divided into ";
     public static final String NOT_EQUALLY = " groups but not equally and  ";
-    public static final String DONE = "Done";
+    public static final String DONE = "partcicipants grouping done";
     public static final String GROUP_NO_GREATER_THAN_PARTICIPANT_NO = "number of groups greater than number of participants " +
         "therefore each group will have one participants, number of groups =  ";
     public static final String AND = " and ";
